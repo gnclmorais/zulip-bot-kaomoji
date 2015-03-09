01f11f050afc9d47f79b4d4f7886c67cbc974f09
Gonçalo Morais <gnclmorais@gmail.com>
2015-03-09 18:23:21 -0400

    Starting dealing with PostgreSQL database.

--- kaomoji.py
+++ kaomoji.py
@@ -1,13 +1,28 @@
 # -*- coding: utf-8 -*-
 import os
+import re
 import zulip
 import json
 import requests
+import psycopg2
+import urlparse
+
+import pprint
 
 
 class KaomojiBot():
     def __init__(self, zulip_usr, zulip_api, private_usr, private_api,
                  command, help, kaomojis, subscribed_streams=[]):
+        self.api_key_size = 32
+        self.table = 'keys'
+        self.remove_commands = [
+            'delete',
+            'remove',
+            'exit',
+            'stop']
+
+        self.connect('postgres://hvctpbozzdoxdy:sI6zqEvx_MEENyRfikUcquZYQg@ec2-23-21-183-70.compute-1.amazonaws.com:5432/ddn7lbp8frv36f')
+
         self.username = zulip_usr
         self.api_key = zulip_api
         self.private_username = private_usr
@@ -59,10 +74,18 @@ class KaomojiBot():
     picks a caption, and calls send_message()
     '''
     def respond(self, msg):
+
+        # Check if it’s a private message;
+        # If so, it’s probably someont sending us an e-mail address and API key
+        if msg['type'] == 'private':
+            self._handle_pm(msg)
+            return
+
         # Proceed only if we find an instance of the command and there is
         # a following keywork (that hopefully corresponds to a kaomoji).
         content = msg['content'].strip().encode('utf-8')
 
+        # Check if the message is a cry for help
         if content == self.help:
             self.send_help(msg)
             return
@@ -88,6 +111,43 @@ class KaomojiBot():
                 new_msg.append(keyword)  # Not a keyword, so put it back
         if len(new_msg):
             self.send_message(msg, " ".join(new_msg))
+
+    def _handle_pm(self, msg):
+        address = msg['sender_email']
+        command = msg['content'].split(' ')
+
+        # Just 2 arguments: cool
+        # More than 2 args: too much info, warn the user
+        # Less than 2 args: ¯\_(ツ)_/¯
+        if len(command) == 1:
+            command = str(command[0])
+            # if len(cmnd) == self.api_key_size and re.match('^\w+$', cmnd):
+            #     self._user_store(mail, cmnd)
+            # elif cmnd in self.remove_commands:
+            #     self._user_remove(mail)
+            # else:
+            #     self.send_private_message(mail,
+            #                               'Error! Please review your command.')
+
+            print('Printing:')
+            print(self.db_search(command))
+        else:
+            # TODO Send message to that e-mail asking for more info
+            self.send_private_message(address, 'Error! Way too many arguments…')
+
+    '''
+    Stores a pair (key, value),
+    where the `key` is the user’s e-mail and `value` is the API key
+    '''
+    def _user_store(self, address, api_key):
+        ## TODO Check if this `address` already has something saved
+        self.db_insert(address, api_key)
+        msg = 'Successfuly stored {0}'.format(api_key)
+        self.send_private_message(address, msg)
+
+    def _user_remove(self, address):
+        ## TODO Check if this `address` already has something saved
+        self.db_remove(address)
 
     '''
     Sends available commands
@@ -133,6 +193,50 @@ Available keywords & correspondent kaomojis:
                                                         self.private_api_key))
 
     '''
+    Connect to database
+    '''
+    def connect(self, database_url):
+        urlparse.uses_netloc.append("postgres")
+        url = urlparse.urlparse(database_url)
+
+        try:
+            self.conn = psycopg2.connect(
+                database=url.path[1:],
+                user=url.username,
+                password=url.password,
+                host=url.hostname,
+                port=url.port)
+            self.cur = self.conn.cursor()
+        except:
+            print "I am unable to connect to the database"
+
+    def db_search(self, mail):
+        query = """SELECT email, api_key
+                   FROM {0}
+                   WHERE email = \'{1}\';""".format(self.table, mail)
+        return self._db_execute_query(query)
+
+    def db_insert(self, mail, key):
+        query = """INSERT INTO {0} (email, api_key)
+                   VALUES (\'{1}\', \'{2}\')""".format(self.table, mail, key)
+        return self._db_execute_query(query)
+
+    def db_update(self, mail, key):
+        query = """UPDATE {0}
+                   SET api_key = \'{1}\'
+                   WHERE email = \'{2}\'""".format(self.table, key, mail)
+        return self._db_execute_query(query)
+
+    def db_remove(self, mail):
+        query = """DELETE FROM {0}
+                   WHERE email = \'{1}\'""".format(self.table, mail)
+        return self._db_execute_query(query)
+
+    def _db_execute_query(self, query):
+        self.cur.execute(query)
+        return self.cur.fetchall()
+
+    '''
     Blocking call that runs forever.
     Calls self.respond() on every message received.
     '''
@@ -147,6 +251,7 @@ zulip_usr = os.environ['ZULIP_USR']
 zulip_api = os.environ['ZULIP_API']
 private_usr = os.environ['ZULIP_PRIVATE_USR']
 private_api = os.environ['ZULIP_PRIVATE_API']
+database_url = os.environ['DATABASE_URL']
 '''
 Recognised command:
 '''
